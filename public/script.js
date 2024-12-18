ea6c5c053fea8c053c5bce1dc57f225159b65015
Mrjey01 <jonataslautenschlager@hotmail.com>
2024-12-18 12:12:32 +0100

    Suppression des fichiers inutiles

--- public/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("actionButton");
-    button.addEventListener("click", () => {
-        alert("Bonjour ! Vous avez cliqué sur le bouton.");
-    });
-});
